222a3581335c2ec01dbb6a68bc5f49169b2c017f
zed <zy@example.com>
2017-01-21 16:08:52 -0500

    improve cli

--- echo_cli.cpp
+++ echo_cli.cpp
@@ -19,20 +19,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <thread>
+#include <vector>
+
+#define THRNUM 1
 
 using namespace std;
 
 char readbuf[1000];
 
 void read_func(int fd){
+	cout << "starting read thread" << endl;
 	while(true){
 		int ret = read(fd, readbuf, sizeof(readbuf));
 		if(ret < 0){
 			cout << strerror(errno) << endl;
-			continue;
+			if(errno != EINTR || errno != EAGAIN) break;
 		}
 		else if(ret == 0){
 			close(fd);
+			cout << "read EOF" << endl;
 			break;
 		}
 		else{
@@ -41,6 +46,32 @@ void read_func(int fd){
 		}
 
 	}
+	cout << "exiting read thread" << endl;
+}
+
+string gen_str(){
+	char c = 'A' + rand()%26;
+	int len = 4 + rand()%16;
+	return string(len, c);
+}
+void write_func(int fd){
+	while(true){
+		string line = gen_str();
+		cout << "writing " << line << endl;
+		int nw = 0;
+		while(true){
+			cout << " writing " << nw << endl;
+			int ret = write(fd, line.c_str()+nw, line.size() - nw);
+			if(ret<0){
+				if(errno != EINTR || errno != EAGAIN) break;
+			}
+			if(ret == 0) break;
+			nw += ret;
+		}
+		sleep(1);
+
+	}
+
 }
 
 int main(int argc, char** argv){
@@ -60,19 +91,18 @@ int main(int argc, char** argv){
 		exit(-1);
 	}
 
-	thread read_th(read_func, sockfd);
-
-	string line;
-	while(getline(std::cin, line)){
-		int ret = write(sockfd, line.c_str(), line.size());
-		if(ret != (int)line.size()){
-			cout << "write error" << endl;
-			break;
-		}
-
+	//thread read_th(read_func, sockfd);
+	vector<thread> reader;
+	vector<thread> writer;
+	for(int i = 0; i < THRNUM; ++i) {
+		reader.push_back(thread(read_func, sockfd));
+		writer.push_back(thread(write_func, sockfd));
 	}
-	shutdown(sockfd, SHUT_WR);
-	read_th.join();
+	for(int i = 0; i < THRNUM; ++i) {
+		reader[i].join();
+		writer[i].join();
+	}
+	close(sockfd);
 }
 
 
